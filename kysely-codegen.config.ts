5f5f50c4f738cc2b2cd290c6f27b44554013e1d6
Piyush-Arora03 <piyush.2023ug1113@iiitranchi.ac.in>
2025-12-05 16:54:28 +0530

    added debug_log schema

--- kysely-codegen.config.ts
+++ kysely-codegen.config.ts
@@ -2,7 +2,7 @@ export default {
   dialect: 'postgres',
   // Output settings
   outFile: 'lib/db/generated/db-types.ts',
-  schema: ['public'],
+  schema: ['public','debug_log'],
   camelCase: false,
   exportMode: 'named',
   strictMode: true
